0ffe0e6e6f5e2dedb1931eab10e1c084720fa40d
cjara002 <cjjara002@gmail.com>
2026-04-05 09:29:59 -0700

    Made my browse component. Now the front end connects to the backend which connects to the PokeApi

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -6,10 +6,23 @@ import {
 } from '@angular/core';
 import { PokemonApiService } from '../../services/pokemon-api.service';
 import { Router } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-browse',
-  imports: [],
+  imports: [
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    TitleCasePipe
+  ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,5 +39,21 @@ export class BrowseComponent implements OnInit {
 
   onPokemonSelected(name: string): void {
     this.#router.navigate(['/pokemon', name]);
+  }
+
+  onNextPage(): void {
+    const current = this.#pokemonService.searchParams();
+    this.#pokemonService.loadPokemonList(
+      current.limit,
+      current.offset + current.limit
+    );
+  }
+
+  onPreviousPage(): void {
+    const current = this.#pokemonService.searchParams();
+    this.#pokemonService.loadPokemonList(
+      current.limit,
+      Math.max(0, current.offset - current.limit)
+    )
   }
 }
